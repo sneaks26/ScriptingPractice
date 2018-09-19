76f93df5142f1394898b282e2bf057d29779a23c
Shuyen <shuyenphoon@gmail.com>
2018-09-19 20:45:07 +1000

    End of Class

--- Assets/Scripts/ExampleScript_Shu.cs
+++ Assets/Scripts/ExampleScript_Shu.cs
@@ -10,7 +10,8 @@ public class ExampleScript_Shu : MonoBehaviour {
     public int aWholeNumber = 20;
     public string firstName = "sneaks";
     public int playerHealth;
-
+    public float force = 10;
+    public Color materialColor;
 
 	// Use this for initialization
 	void Start () {
@@ -19,6 +20,35 @@ public class ExampleScript_Shu : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+
+        // Made the cube spin
         transform.eulerAngles += new Vector3 (0, speed, 0);
+
+        // Cube is moving to the right
+        //transform.position += new Vector3(speed, 0, 0);
+
+        // Cube rotates on z axis
+        //transform.Rotate(new Vector3(0, 0, speed));
+
+        // Cube rotates on x axis
+        //transform.Rotate(new Vector3(speed, 0, 0));
+
+       
+
+   }
+
+    // Rigidbody force - new function - 
+    public void Jump()
+    {
+        GetComponent<Rigidbody>().AddForce(0, force, 0);
+
+
+    }
+        // Fade opacity of object
+        public void Fade(float sliderValue) {
+            materialColor.a = sliderValue;
+            GetComponent<MeshRenderer>().material.color = materialColor;
+
+
     }
 }
