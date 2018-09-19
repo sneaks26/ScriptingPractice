1171d400654f9d575a43da4d45fceb38b85f5c0c
Shuyen <shuyenphoon@gmail.com>
2018-09-19 19:36:02 +1000

    Changesss
    made some scripts happen.

---
+++ Assets/Scripts/ExampleScript_Shu.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExampleScript_Shu : MonoBehaviour {
+
+    public bool trueOrFalse;
+    public float decimalNumber = 5.0f;
+    public float speed = 1.0f;
+    public int aWholeNumber = 20;
+    public string firstName = "sneaks";
+    public int playerHealth;
+
+
+	// Use this for initialization
+	void Start () {
+        Debug.Log("hello boo!");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.eulerAngles += new Vector3 (0, speed, 0);
+    }
+}
